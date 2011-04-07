a3553a71e4ae2ef0e4587bf03f464e2c816cb631
Leonardo Saraiva <vyper@maneh.org>
2011-04-07 00:45:57 -0300

    adding crud for Coluna

--- AltaMontanha/Controllers/ManterColunaController.cs
+++ AltaMontanha/Controllers/ManterColunaController.cs
@@ -3,88 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AltaMontanha.Models.Dominio;
+using AltaMontanha.Models.Fachada;
 
 namespace AltaMontanha.Controllers
 {
-    public class ManterColunaController : Controller
-    {
-        //
-        // GET: /ManterColuna/
-        public ActionResult Index()
-        {
-            return View();
-        }
-        //
-        // GET: /ManterColuna/VisualizarUsuario/5
-        public ActionResult PesquisarColuna(int id)
-        {
-            return View();
-        }
-        //
-        // GET: /ManterColuna/CadastrarUsuario
-        public ActionResult CarregarColuna()
-        {
-            return View();
-        } 
-        //
-        // POST: /ManterColuna/CadastrarUsuario
-        [HttpPost]
-		public ActionResult CarregarColuna(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+	public class ManterColunaController : Controller
+	{
+		ConteudoFacade facade = new ConteudoFacade();
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-        //
-        // GET: /ManterColuna/AlterarUsuario/5
-        public ActionResult SalvarColuna(int id)
-        {
-            return View();
-        }
-        //
-        // POST: /ManterColuna/AlterarUsuario/5
-        [HttpPost]
-		public ActionResult SalvarColuna(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-        //
-        // GET: /ManterColuna/ExcluirUsuario/5
-		public ActionResult ExcluirColuna(int id)
-        {
-            return View();
-        }
-        //
-        // POST: /ManterColuna/ExcluirUsuario/5
-        [HttpPost]
-		public ActionResult ExcluirColuna(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+		//
+		// GET: /ManterColuna/
+		public ActionResult Index()
+		{
+			IList<Coluna> colunas = facade.PesquisarColuna(null);
+			return View(colunas);
+		}
+		//
+		// GET: /ManterColuna/CadastrarColuna
+		public ActionResult CadastrarColuna()
+		{
+			// TODO: incluir autor
+			return View();
+		}
+		//
+		// POST: /ManterColuna/CadastrarColuna
+		[HttpPost]
+		public ActionResult CadastrarColuna(Coluna coluna)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					// TODO: incluir autor
+					facade.SalvarColuna(coluna);
+					return RedirectToAction("Index");
+				}
+				else
+				{
+					return View(coluna);
+				}
+			}
+			catch
+			{
+				return View(coluna);
+			}
+		}
+		//
+		// GET: /ManterColuna/AlterarColuna/5
+		public ActionResult AlterarColuna(int Codigo)
+		{
+			// TODO: incluir autor
+			// TODO: implementar sobrecarga
+			return View(facade.PesquisarColuna(new Coluna() { Codigo = Codigo }));
+		}
+		//
+		// POST: /ManterColuna/AlterarColuna/5
+		[HttpPost]
+		public ActionResult AlterarColuna(Coluna coluna)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					// TODO: incluir autor
+					facade.SalvarColuna(coluna);
+					return RedirectToAction("Index");
+				}
+				else
+				{
+					return View(coluna);
+				}
+			}
+			catch
+			{
+				return View(coluna);
+			}
+		}
+		//
+		// GET: /ManterColuna/ExcluirColuna/5
+		public ActionResult ExcluirColuna(int Codigo)
+		{
+			facade.ExcluirColuna(Codigo);
+			return RedirectToAction("Index");
+		}
+	}
 }
