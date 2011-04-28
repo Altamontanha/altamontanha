e281f09e1243351641dbb598326a7a813caf5cd9
Leonardo Saraiva <vyper@maneh.org>
2011-04-28 16:42:13 -0300

    removendo campos nao utilizados de tb_perfil

---
+++ db/007-remover-campos-de-permissao-da-tb_perfil.sql
@@ -0,0 +1,7 @@
+alter table tb_perfil drop column noticia;
+alter table tb_perfil drop column link;
+alter table tb_perfil drop column foto;
+alter table tb_perfil drop column coluna;
+alter table tb_perfil drop column banner;
+alter table tb_perfil drop column aventura;
+alter table tb_perfil drop column artigo;
