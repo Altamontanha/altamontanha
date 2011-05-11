124e29cd1c293f0e7302badb000033b498193665
Leonardo Saraiva <vyper@maneh.org>
2011-05-10 23:11:48 -0300

    adicionando controller do manterbanner

---
+++ AltaMontanha/Controllers/ManterBannerController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AltaMontanha.Models.Dominio;
+using AltaMontanha.Models.Fachada;
+
+namespace AltaMontanha.Controllers
+{
+    public class ManterBannerController : Controller
+    {
+		private MultimidiaFacade facade = new MultimidiaFacade();
+
+        //
+        // GET: /Banner/
+        public ActionResult Index()
+        {
+			IList<Banner> banners = facade.PesquisarBanner(null);
+            return View(banners);
+        }
+
+		//
+        // GET: /ManterBanner/CadastrarBanner
+		public ActionResult CadastrarBanner()
+        {
+            return View();
+        } 
+
+        //
+		// POST: /ManterBanner/CadastrarBanner
+        [HttpPost]
+		public ActionResult CadastrarBanner(Banner banner)
+        {
+            try
+            {
+				facade.SalvarBanner(banner);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(banner);
+            }
+        }
+        
+        //
+		// GET: /ManterBanner/AlterarBanner/5
+		public ActionResult AlterarBanner(int Codigo)
+        {
+			Banner banner = facade.PesquisarBanner(Codigo);
+            return View(banner);
+        }
+
+        //
+		// POST: /ManterBanner/AlterarBanner/5
+        [HttpPost]
+		public ActionResult AlterarBanner(Banner banner)
+        {
+            try
+            {
+				facade.SalvarBanner(banner);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(banner);
+            }
+        }
+
+        //
+        // GET: /ManterBanner/ExluirBanner/5
+        public ActionResult ExcluirBanner(int Codigo)
+        {
+			facade.ExcluirBanner(Codigo);
+			return RedirectToAction("Index");
+        }
+    }
+}
