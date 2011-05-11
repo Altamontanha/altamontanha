55eefef88f55b5006101314d417f9dc7c931038b
Leonardo Saraiva <vyper@maneh.org>
2011-05-10 23:41:16 -0300

    adicionando validacoes e removendo atributo de data

--- AltaMontanha/Models/Dominio/Foto.cs
+++ AltaMontanha/Models/Dominio/Foto.cs
@@ -9,12 +9,24 @@ namespace AltaMontanha.Models.Dominio
 	public class Foto
 	{
 		public virtual int Codigo { get; set; }
-		public virtual string Autor { get; set; }
-		public virtual string Legenda { get; set; }
-		public virtual string Caminho { get; set; }
-		public virtual string Fonte { get; set; }
-		public virtual DateTime Data { get; set; }
 
+		[Required(ErrorMessage = "Autor é um campo obrigatório")]
+		[StringLength(150)]
+		public virtual string Autor { get; set; }
+
+		[Required(ErrorMessage = "Legenda é um campo obrigatório")]
+		[StringLength(50)]
+		public virtual string Legenda { get; set; }
+
+		[Required(ErrorMessage = "Caminho é um campo obrigatório")]
+		[StringLength(255)]
+		public virtual string Caminho { get; set; }
+
+		[Required(ErrorMessage = "Fonte é um campo obrigatório")]
+		[StringLength(75)]
+		public virtual string Fonte { get; set; }
+
+		// TODO: remover?
 		public Foto Clone()
 		{
 			return (Foto) this.MemberwiseClone();
