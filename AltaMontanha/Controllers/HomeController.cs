f5c69c3c7f83a31714c56546483ee110558ce5e2
otaviano <otavianomontes@gmail.com>
2011-05-20 15:07:18 -0300

    Criado Helpers de artigo, Aventura Coluna e Noticia. Obs.: Não está realizando as consultas e possui TODO'S de caminho de foto e javascript

--- AltaMontanha/Controllers/HomeController.cs
+++ AltaMontanha/Controllers/HomeController.cs
@@ -12,7 +12,12 @@ namespace AltaMontanha.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
-
+			ViewData["ListaNoticiasDestaque"] = new List<Models.Dominio.Noticia>();
+			ViewData["ListaNoticias"] = new List<Models.Dominio.Noticia>();
+			ViewData["ListaColunas"] = new List<Models.Dominio.Coluna>();
+			ViewData["ListaArtigos"] = new List<Models.Dominio.Artigo>();
+			ViewData["ListaArtigosHistoria"] = new List<Models.Dominio.Artigo>();
+			ViewData["ListaAventuras"] = new List<Models.Dominio.Aventura>();
 
             return View();
         }
