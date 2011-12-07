83a7dc14654190b8007c87c1c2bc1da8a80f13de
otaviano <otavianomontes@gmail.com>
2011-12-07 15:42:20 -0200

    issue #37 closed

--- AltaMontanha/Models/Dominio/Conteudo.cs
+++ AltaMontanha/Models/Dominio/Conteudo.cs
@@ -17,7 +17,6 @@ namespace AltaMontanha.Models.Dominio
 		/// Código do conteúdo
 		/// </summary>
 		public virtual int Codigo { get; set; }
-
 		/// <summary>
 		/// Slug do título utilizado para url amigável 
 		/// </summary>
@@ -32,55 +31,46 @@ namespace AltaMontanha.Models.Dominio
 				return slug.ToLower();
 			}
 		}
-
 		/// <summary>
 		/// Título do conteúdo
 		/// </summary>
 		[Required(ErrorMessage = "Título é um campo obrigatório")]
 		[StringLength(150)]
 		public virtual string Titulo { get; set; }
-
 		/// <summary>
 		/// Ante-título do conteúdo
 		/// </summary>
 		[Required(ErrorMessage = "Ante-título é um campo obrigatório")]
 		[StringLength(250)]
 		public virtual string AnteTitulo { get; set; }
-
 		/// <summary>
 		/// Resumo do conteúdo
 		/// </summary>
 		[Required(ErrorMessage = "Resumo é um campo obrigatório")]
 		public virtual string Resumo { get; set; }
-
 		/// <summary>
 		/// Texto (em html) do conteúdo
 		/// </summary>
 		[Required(ErrorMessage = "Texto é um campo obrigatório")]
 		public virtual string Texto { get; set; }
-
 		/// <summary>
 		/// Data da postagem do conteúdo
 		/// </summary>
 		[Required(ErrorMessage = "Data é um campo obrigatório")]
 		[DataType(DataType.Date, ErrorMessage="Formato da data é inválido")]
 		public virtual DateTime Data { get; set; }
-		
 		/// <summary>
 		/// Lista de palavras-chave do conteúdo
 		/// </summary>
 		public virtual IList<PalavraChave> ListaPalavrasChave { get; set; }
-
 		/// <summary>
 		/// Lista de fotos do conteúdo
 		/// </summary>
 		public virtual IList<Foto> ListaFotos { get; set; }
-
 		/// <summary>
 		/// Foto utilizada como capa do conteúdo
 		/// </summary>
 		public virtual Foto FotoCapa { get; set; }
-		
 		/// <summary>
 		/// Usuário que fez o cadastro do conteúdo
 		/// </summary>
